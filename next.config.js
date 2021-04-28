62577a7b22fab52dad31ac0fc0eb625e00f92232
Taw Ing <lems3t2@gmail.com>
2021-04-29 03:07:49 +0800

    Netlify error o"using build directory: /opt/build/repo/.next"

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = {
     storageBucket: "products-1da2b.appspot.com",
     messagingSenderId: "124233021458",
     appId: "1:124233021458:web:7b6814d07dac6c214ac0cb"
-  }
+  },
+  distDir: 'build',
 }
